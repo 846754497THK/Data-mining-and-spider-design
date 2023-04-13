1abece81f17253a682f195612419c89c6bda2279
846754497THK <846754497@qq.com>
2023-04-13 20:35:07 +0800

    upgrade

---
+++ example2/test/1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int INF=0x3f3f3f3f;
+int main(){
+	double a,b,c,a1,b1,c1;
+	scanf("%lf%lf%lf",&a,&b,&c);
+	a1=1/a;
+	b1=1/b;
+	c1=1/c;
+	cout<<a1<<endl;
+	cout<<b1<<endl;
+	cout<<c1<<endl;
+	cout<<a1+b1+c1;
+
+	return 0;
+}
